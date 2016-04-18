465f4b3a1e6fc44a982d501680016be32c26a685
aniaw <ania_w@outlook.com>
2016-04-18 11:28:18 +0200

    invalid

--- app/filter.js
+++ app/filter.js
@@ -3,38 +3,15 @@
     'use strict';
     var app = angular.module('app', []);
 
-    app.filter('quote', function ()
-    {
-        return function (text)
-        {
-            return '"' + text + '"';
-        };
-    });
 
-    app.filter('withoutH', function ()
+    app.controller("FilterCtrl", function ($scope)
     {
-        return function (text)
-        {
-            return text.split('h').join('').split('H').join('');
-        };
-    });
-
-    app.filter('firstLetterUp', function ()
-    {
-        return function (text)
-        {
-            return text.replace(/([^\W_]+[^\s-]*) */g, function (text)
-            {
-                return text.charAt(0).toUpperCase() + text.substr(1).toLowerCase();
-            });
-        };
-    });
-
-
-    app.controller('FilterCtrl', function ($scope)
-    {
+        var a;
+        if (a == a) {
+            return a;
+        }
         $scope.data = {
-            citation: 'To be, or not to be...',
+            citation: "To be, or not to be...",
             title: 'The Hobbit: The Battle of the Five Armies',
             someText: 'Visit News for up-to-the-minute news, breaking news, video, audio and stories.'
         };
